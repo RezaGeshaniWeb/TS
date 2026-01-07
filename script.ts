79fe0ad0190a0deb36dea44eee7262ff7e29e6cd
Reza Geshani <rezageshaniweb@gmail.com>
2026-01-07 23:50:40 +0330

    data types in ts

--- script.ts
+++ script.ts
@@ -1,3 +1,14 @@
+// Data Types
 const message: string = "Hello TS"
+let myName: string = "Reza Geshani"
+let score: number = 20
+let isProgrammer: boolean = true
 
-console.log(message)
+let numbers: number[] = [2, 4, 6, 8, 10]
+let cities: Array<string> = ["tehran", "hamedan", "sanandaj", "esfahan"]
+
+let persons: [string, number] = ["reza", 25]  // touple
+
+let info: { name: string, age: number } = { name: 'reza', age: 25 }
+
+let randomValue: any = "value"

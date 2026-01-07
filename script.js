5a78b0fe436e94d88fcfaebbc9ab2f8742a70549
Reza Geshani <rezageshaniweb@gmail.com>
2026-01-07 23:53:14 +0330

    compile ts

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+// Data Types
 var message = "Hello TS";
-console.log(message);
+var myName = "Reza Geshani";
+var score = 20;
+var isProgrammer = true;
+var numbers = [2, 4, 6, 8, 10];
+var cities = ["tehran", "hamedan", "sanandaj", "esfahan"];
+var persons = ["reza", 25]; // touple
+var info = { name: 'reza', age: 25 };
+var randomValue = "value";
